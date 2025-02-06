8dc6cd218ec53617f2ae973725bcea2f9379df2f
Waqas Naseer <waqas.naseer@nextbridge.org>
2025-02-06 17:25:47 +0500

    Site opencart changed

--- src/main/java/stepDefinitions/loginStepDefinition.java
+++ src/main/java/stepDefinitions/loginStepDefinition.java
@@ -20,7 +20,7 @@ public class loginStepDefinition
     {
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
-        driver.get("https://classic.freecrm.com/");
+        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
         driver.manage().window().maximize();
     }
     @When("^Title of Login Page is Free CRM$")
@@ -29,19 +29,19 @@ public class loginStepDefinition
         String title=driver.getTitle();
         System.out.println("Title is: "+title);
 
-        Assert.assertEquals("Free CRM Software for customer relationship management, sales and support.",title);
+        Assert.assertEquals("Account Login",title);
     }
     @Then("^User enters username and Password$")
     public void user_enters_username_and_Password()
     {
-        driver.findElement(By.name("username")).sendKeys("naveenk");
-        driver.findElement(By.name("password")).sendKeys("test@123");
+        driver.findElement(By.name("email")).sendKeys("Testerqaw@youpmail.com");
+        driver.findElement(By.name("password")).sendKeys("Playwright@123");
     }
 
     @Then("^User clicks on Login Button$")
     public void user_clicks_on_Login_Button()
     {
-        WebElement loginBtn=driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/form/div/div/input"));
+        WebElement loginBtn=driver.findElement(By.xpath("//input[@value=\"Login\"]"));
         loginBtn.click();
     }
 
@@ -51,7 +51,7 @@ public class loginStepDefinition
         String homePageTitle=driver.getTitle();
         System.out.println("Home Page Title is: "+homePageTitle);
 
-        Assert.assertEquals("CRMPRO",homePageTitle);
+        Assert.assertEquals("My Account",homePageTitle);
     }
 
 }
